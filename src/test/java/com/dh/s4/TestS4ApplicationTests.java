f330ef0fc5041d5bd8da53e0a284c61ccb93c4e6
Yerson Copa <yerson.copa@gmail.com>
2021-03-17 16:10:13 -0400

    basic structure for spring project

---
+++ src/test/java/com/dh/s4/TestS4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.s4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestS4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
